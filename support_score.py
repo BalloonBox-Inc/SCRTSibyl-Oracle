21e86eb0e99af82a5089c8a76abe0fba99677856
Irene Fabris <95246024+irene-bbox@users.noreply.github.com>
2022-02-03 17:25:19 -0800

    Add Plaid model files

---
+++ support_score.py
@@ -0,0 +1,31 @@
+# Import libraries
+import time
+from support_models import *
+from support_metrics import *
+
+
+# Plaid
+def main(tx):
+
+    credit = plaid_credit(tx)
+    velocity = plaid_velocity(tx)
+    stability = plaid_stability(tx)
+    diversity = plaid_diversity(tx)
+
+    score = 600*(0.45*credit + 0.35*velocity + 0.15*stability + 0.05*diversity)+300 #range score between [300, 900]
+    return score, feedback
+
+
+
+# Define path to locally stored real users data. Next, import data
+path_dir=PATH_REAL_UDERS_DATA
+
+# Calculate score for all users you have data for
+for user in [1,2,3,4,5,6,7,8,9,11,12]:
+    start_time = time.time()
+    tx = get_tx(path_dir, user)
+    score, feedback = main(tx)
+    runtime = round(time.time() - start_time, 3)
+    print('User #{} got a SCRTSybil score (computed in {} sec) of {}/900 points'.format(user, runtime, round(score)))
+
+
