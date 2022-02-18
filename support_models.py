a986d5259d1837aa584b2f81333bc511547b9ecd
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-02-18 15:23:35 -0800

    add feedback

--- support_models.py
+++ support_models.py
@@ -1,59 +1,57 @@
 from support_metrics_plaid import *
 
 
-def plaid_credit(tx):
+def plaid_credit(txn, feedback):
 
-    mix = credit_mix(tx)
-    lim = credit_limit(tx)
-    util = credit_util_ratio(tx)
-    interest = credit_interest(tx)
-    leng = credit_length(tx)
-    liv = credit_livelihood(tx)
+    mix = credit_mix(txn, feedback)
+    limit = credit_limit(txn, feedback)
+    util_ratio = credit_util_ratio(txn, feedback)
+    interest = credit_interest(txn, feedback)
+    length = credit_length(txn, feedback)
+    livelihood = credit_livelihood(txn, feedback)
 
     score = 0.05*mix \
-        + 0.15*lim \
-        + 0.80*0.35*util \
+        + 0.15*limit \
+        + 0.80*0.35*util_ratio \
         + 0.80*0.15*interest \
-        + 0.80*0.40*leng \
-        + 0.80*0.10*liv \
+        + 0.80*0.40*length \
+        + 0.80*0.10*livelihood \
 
     return score
 
 
-def plaid_velocity(tx):
+def plaid_velocity(txn, feedback):
 
-    wit = velocity_withdrawals(tx)
-    dep = velocity_deposits(tx)
-    flow = velocity_month_net_flow(tx)
-    cnt = velocity_month_txn_count(tx)
-    slo = velocity_slope(tx)
+    withdrawals = velocity_withdrawals(txn, feedback)
+    deposits = velocity_deposits(txn, feedback)
+    net_flow = velocity_month_net_flow(txn, feedback)
+    txn_count = velocity_month_txn_count(txn, feedback)
+    slope = velocity_slope(txn, feedback)
 
-    score = 0.05*wit \
-        + 0.15*dep \
-        + 0.40*flow \
-        + 0.10*cnt \
-        + 0.30*slo \
-
-    return score
-
-
-def plaid_stability(tx):
-
-    bal = stability_tot_balance_now(tx)
-    run = stability_min_running_balance(tx)
-
-    score = 0.65*bal \
-        + 0.35*run \
+    score = 0.05*withdrawals \
+        + 0.15*deposits \
+        + 0.40*net_flow \
+        + 0.10*txn_count \
+        + 0.30*slope \
 
     return score
 
 
-def plaid_diversity(tx):
+def plaid_stability(txn, feedback):
 
-    acc = diversity_acc_count(tx)
-    pro = diversity_profile(tx)
+    balance = stability_tot_balance_now(txn, feedback)
+    run_balance = stability_min_running_balance(txn, feedback)
 
-    score = 0.60*acc \
-        + 0.40*pro \
+    score = 0.65*balance + 0.35*run_balance
+
+    return score
+
+
+def plaid_diversity(txn, feedback):
+
+    acc_count = diversity_acc_count(txn, feedback)
+    profile = diversity_profile(txn, feedback)
+
+    score = 0.60*acc_count + 0.40*profile
 
     return score
