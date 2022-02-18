77075aff89abc3e96d1ba52e8eb8592a115ca129
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-02-18 15:12:30 -0800

    remove unused functions

--- support_plaid.py
+++ support_plaid.py
@@ -1,11 +1,7 @@
-from plaid.model.investments_transactions_get_request_options import InvestmentsTransactionsGetRequestOptions
-from plaid.model.investments_transactions_get_request import InvestmentsTransactionsGetRequest
-from plaid.model.investments_holdings_get_request import InvestmentsHoldingsGetRequest
 from plaid.model.transactions_get_request_options import TransactionsGetRequestOptions
 from plaid.model.accounts_balance_get_request import AccountsBalanceGetRequest
 from plaid.model.transactions_get_request import TransactionsGetRequest
 from plaid.model.accounts_get_request import AccountsGetRequest
-from plaid.model.identity_get_request import IdentityGetRequest
 
 from plaid.api import plaid_api
 from datetime import timedelta
@@ -49,20 +45,6 @@ def format_error(e):
                 }
             }
 
-def plaid_identity(access_token, client):
-    
-    try:
-        request = IdentityGetRequest(
-            access_token=access_token
-            )
-
-        response = client.identity_get(request).to_dict()
-
-    except plaid.ApiException as e:
-        response = format_error(e)
-    
-    return response
-
 
 def plaid_accounts(access_token, client):
 
@@ -93,21 +75,6 @@ def plaid_balance(access_token, client):
     return response
 
 
-def plaid_holdings(access_token, client):
-
-    try:
-        request = InvestmentsHoldingsGetRequest(
-            access_token=access_token
-            )
-        
-        response = client.investments_holdings_get(request).to_dict()
-
-    except plaid.ApiException as e:
-        response = format_error(e)
-    
-    return response
-
-
 def plaid_transactions(access_token, client, timeframe):
 
     start_date = (datetime.now() - timedelta(days=timeframe))
@@ -127,25 +94,3 @@ def plaid_transactions(access_token, client, timeframe):
         response = format_error(e)
     
     return response
-
-
-def plaid_investment_transactions(access_token, client, timeframe):
-
-    start_date = (datetime.now() - timedelta(days=(timeframe)))
-    end_date = datetime.now()
-
-    try:
-        request = InvestmentsTransactionsGetRequest(
-            access_token=access_token,
-            start_date=start_date.date(),
-            end_date=end_date.date(),
-            options=InvestmentsTransactionsGetRequestOptions()
-            )
-
-        response = client.investments_transactions_get(request).to_dict()
-
-    except plaid.ApiException as e:
-        response = format_error(e)
-    
-    return response
-    
