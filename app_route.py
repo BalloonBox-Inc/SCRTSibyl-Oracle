c6b9c29212574423a246c87d8c1559863bd40698
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-02-18 15:11:14 -0800

    update test keys

--- app_route.py
+++ app_route.py
@@ -2,7 +2,7 @@ from flask import request, make_response
 from dotenv import load_dotenv
 from os import getenv
 
-from support_coinbase import *
+# from support_coinbase import *
 from support_plaid import *
 from support_score import *
 from debugging import *
@@ -17,35 +17,34 @@ def credit_score():
 
     # front-end inputs
     # plaid_token = request.json.get('plaid_public_token', None)
-    plaid_token = getenv('PLAID_ACCESS_TOKEN')
     # coinbase_token = request.json.get('coinbase_public_token', None)
-    coinbase_token = None
-
     # keplr_token = request.json.get('keplr_token', None)
+
+    # plaid_token = getenv('PLAID_ACCESS_TOKEN')
+    plaid_token = getenv('ACCESS_TOKEN')
+    coinbase_token = None
 
     # plaid credit score
     if plaid_token:
         # create client connection
-        client_plaid = plaid_client('sandbox', getenv('PLAID_CLIENT_ID'), getenv('PLAID_SECRET'))
+        # client_plaid = plaid_client('sandbox', getenv('PLAID_CLIENT_ID'), getenv('PLAID_SECRET'))
+        client_plaid = plaid_client('production', getenv('CLIENT_ID'), getenv('SECRET'))
         
         # fetch data
-        plaid_id = plaid_identity(plaid_token, client_plaid)
         plaid_txn = plaid_transactions(plaid_token, client_plaid, 360)
-        plaid_hldg = plaid_holdings(plaid_token, client_plaid)
-        plaid_invt = plaid_investment_transactions(plaid_token, client_plaid, 360)
-
-        # compute score
-        output = plaid_score(plaid_txn, plaid_id, plaid_hldg, plaid_invt)
-
-    # coinbase credit score
-    elif coinbase_token:
-        # create client connection
-        client_coinbase = coinbase_client(getenv('COINBASE_API_KEY'), getenv('COINBASE_API_SECRET'))
-
-        # fetch data
-        coinbase_attr = None
-
-        # compute score
-        output = coinbase_score(coinbase_attr)
+        return plaid_txn
+        # # compute score
+        # output = plaid_score(plaid_txn)
     
-    return make_response(output, 200)
+    # # coinbase credit score
+    # elif coinbase_token:
+    #     # create client connection
+    #     client_coinbase = coinbase_client(getenv('COINBASE_API_KEY'), getenv('COINBASE_API_SECRET'))
+
+    #     # fetch data
+    #     coinbase_attr = None
+
+    #     # compute score
+    #     output = coinbase_score(coinbase_attr)
+    
+    # return make_response(output, 200)
