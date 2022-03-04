0ff7516ce0d486ff45e53e2f3a211fe5c55cfde8
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-03-03 17:21:05 -0800

    create runtime for performance testing

---
+++ runtime.py
@@ -0,0 +1,30 @@
+import time
+
+def timeit(method):
+    def timed(*args, **kw):
+        t0 = time.time()
+        result = method(*args, **kw)
+        t1 = time.time()
+        if 'log_time' in kw:
+            name = kw.get('log_name', method.__name__.upper())
+            kw['log_time'][name] = int((t1 - t0) * 1000)
+        else:
+            # time length
+            span = (t1 - t0) * 1000 # milliseconds
+            if span >= 1000:
+                span = span / 1000 # seconds
+                if span >= 60:
+                    span = str(round(span / 60, 2)) + ' min' # minutes
+                else:
+                    span = str(round(span, 2)) + ' sec' # seconds
+            else:
+                span = str(round(span, 2)) + ' ms' # milliseconds
+            # function name
+            func = method.__name__
+            size = len(func)
+            if size <= 10:
+                print('> %r:\t\t%s' % (func, span))
+            else:
+                print('> %r:\t%s' % (func, span))
+        return result
+    return timed
