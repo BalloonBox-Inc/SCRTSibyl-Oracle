5a0947d126fa66aef6d4c47160bd979e6d6c70a6
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-03-11 17:07:20 -0800

    add front-end response integration

--- app_route.py
+++ app_route.py
@@ -1,5 +1,7 @@
 from flask import request, make_response
 from dotenv import load_dotenv
+from datetime import datetime
+from datetime import timezone
 from os import getenv
 
 from feedback.qualitative_score import *
@@ -26,97 +28,139 @@ def create_feedback_coinbase():
 @measure_time_and_memory
 def credit_score_plaid():
 
-    try:
-        # plaid_token = request.json.get('plaid_public_token', None)
-        # keplr_token = request.json.get('keplr_token', None)
-        plaid_token = getenv('PLAID_ACCESS_TOKEN')
-        plaid_client_id = getenv('PLAID_CLIENT_ID')
-        plaid_client_secret = getenv('PLAID_CLIENT_SECRET')
-    
-    except Exception as e:
-        return str(e)
-
-    try:
-        # client connection
-        client = plaid_client('sandbox', plaid_client_id, plaid_client_secret)
-        # client = plaid_client('production', plaid_client_id, plaid_client_secret)
+    if request.method == 'POST':
+        try:
+            # plaid_token = request.json.get('plaid_public_token', None)
+            # keplr_token = request.json.get('keplr_token', None)
+            plaid_token = getenv('PLAID_ACCESS_TOKEN')
+            plaid_client_id = getenv('PLAID_CLIENT_ID')
+            plaid_client_secret = getenv('PLAID_CLIENT_SECRET')
         
-        # data fetching and formatting
-        plaid_txn = plaid_transactions(plaid_token, client, 360)
-        plaid_txn = {k:v for k,v in plaid_txn.items() if k in ['accounts','transactions']}
-        plaid_txn['transactions'] = [t for t in plaid_txn['transactions'] if not t['pending']]
+        except Exception as e:
+            return str(e)
         
-        # compute score
-        feedback = create_feedback_plaid()
-        score, feedback = plaid_score(plaid_txn, feedback)
-        msg = qualitative_feedback_plaid(score, feedback)
+        try:
+            # client connection
+            client = plaid_client('sandbox', plaid_client_id, plaid_client_secret)
+            # client = plaid_client('production', plaid_client_id, plaid_client_secret)
+            
+            # data fetching and formatting
+            plaid_txn = plaid_transactions(plaid_token, client, 360)
+            plaid_txn = {k:v for k,v in plaid_txn.items() if k in ['accounts','transactions']}
+            plaid_txn['transactions'] = [t for t in plaid_txn['transactions'] if not t['pending']]
+            
+            # compute score
+            feedback = create_feedback_plaid()
+            score, feedback = plaid_score(plaid_txn, feedback)
+            message = qualitative_feedback_plaid(score, feedback)
 
-        return score, feedback, msg
-    
-    except Exception as e:
-        return 'Error', str(e)
+            status_code = 200
+            status = 'Good'
+        
+        except Exception as e:
+            status_code = 400
+            status = 'Error'
+            score = 0
+            feedback = {}
+            message = str(e)
+        
+        finally:
+            timestamp = datetime.now(timezone.utc).timestamp()
+            output = {
+                'endpoint': '/credit_score/plaid',
+                'title': 'Credit Score',
+                'status_code': status_code,
+                'status': status,
+                'timestamp': timestamp,
+                'score': score,
+                'feedback': feedback,
+                'message': message
+                }
+        
+        return make_response(output, output['status_code'])
 
 
 # @app.route('/credit_score/coinbase', methods=['POST'])
 @measure_time_and_memory
 def credit_score_coinbase():
 
-    try:
-        # coinbase_token = request.json.get('coinbase_public_token', None)
-        # coinmarketcap_key = request.json.get('coinmarketcap_key', None)
-        # keplr_token = request.json.get('keplr_token', None)
-        coinbase_token = getenv('COINBASE_CLIENT_ID')
-        coinbase_secret = getenv('COINBASE_CLIENT_SECRET')
-        coinmarketcap_key = getenv('COINMARKETCAP_KEY')
-    
-    except Exception as e:
-        return str(e)
-    
-    try:
-        # client connection
-        client = coinbase_client(coinbase_token, coinbase_secret)
-
-        # coinmarketcap
-        # fetch top X cryptos from coinmarketcap API
-        top_coins = coinmarketcap_coins(coinmarketcap_key, 50)
-        currencies = coinbase_currencies(client)
-        odd_fiats = ['BHD', 'BIF', 'BYR', 'CLP', 'DJF', 'GNF', 'HUF', 'IQD', 'ISK', 'JOD', 'JPY', 'KMF', 'KRW', 'KWD', 'LYD', 'MGA', 'MRO', 'OMR', 'PYG', 'RWF', 'TND', 'UGX', 'VND', 'VUV', 'XAF', 'XOF', 'XPF']
-        currencies = {k:1 for (k,v) in currencies.items() if v == 0.01 or k in odd_fiats}
-        top_coins.update(currencies)
-        coins = list(top_coins.keys())
-
-        # change coinbase native currency to USD
-        native = coinbase_native_currency(client)
-        if native != 'USD':
-            coinbase_set_native_currency(client, 'USD')
+    if request.method == 'POST':
+        try:
+            # coinbase_token = request.json.get('coinbase_public_token', None)
+            # coinmarketcap_key = request.json.get('coinmarketcap_key', None)
+            # keplr_token = request.json.get('keplr_token', None)
+            coinbase_token = getenv('COINBASE_CLIENT_ID')
+            coinbase_secret = getenv('COINBASE_CLIENT_SECRET')
+            coinmarketcap_key = getenv('COINMARKETCAP_KEY')
         
-
-        # fetch and format data from user Coinbase account
-        coinbase_acc = coinbase_accounts(client)
-        coinbase_acc = [n for n in coinbase_acc if n['currency'] in coins]
+        except Exception as e:
+            return str(e)
         
-        coinbase_txn = [coinbase_transactions(client, n['id']) for n in coinbase_acc]
-        coinbase_txn = [x for n in coinbase_txn for x in n]
-        # keep only certain transaction types
-        txn_types = ['fiat_deposit', 'request', 'buy', 'fiat_withdrawal', 'vault_withdrawal', 'sell', 'send']
-        coinbase_txn = [n for n in coinbase_txn if n['status'] == 'completed' and n['type'] in txn_types]
-        for d in coinbase_txn:
-            # If the txn is of 'send' type and is a credit, then relabel its type to 'send_credit'
-            if d['type']=='send' and np.sign(float(d['amount']['amount']))==1:
-                d['type'] = 'send_credit'
-            # If the txn is of 'send' type and is a debit, then relabel its type to 'send_debit' 
-            elif d['type']=='send' and np.sign(float(d['amount']['amount']))==-1:
-                d['type'] = 'send_debit'
+        try:
+            # client connection
+            client = coinbase_client(coinbase_token, coinbase_secret)
+
+            # coinmarketcap
+            # fetch top X cryptos from coinmarketcap API
+            top_coins = coinmarketcap_coins(coinmarketcap_key, 50)
+            currencies = coinbase_currencies(client)
+            odd_fiats = ['BHD', 'BIF', 'BYR', 'CLP', 'DJF', 'GNF', 'HUF', 'IQD', 'ISK', 'JOD', 'JPY', 'KMF', 'KRW', 'KWD', 'LYD', 'MGA', 'MRO', 'OMR', 'PYG', 'RWF', 'TND', 'UGX', 'VND', 'VUV', 'XAF', 'XOF', 'XPF']
+            currencies = {k:1 for (k,v) in currencies.items() if v == 0.01 or k in odd_fiats}
+            top_coins.update(currencies)
+            coins = list(top_coins.keys())
+
+            # change coinbase native currency to USD
+            native = coinbase_native_currency(client)
+            if native != 'USD':
+                coinbase_set_native_currency(client, 'USD')
             
+
+            # fetch and format data from user Coinbase account
+            coinbase_acc = coinbase_accounts(client)
+            coinbase_acc = [n for n in coinbase_acc if n['currency'] in coins]
             
-        # reset native currency
-        coinbase_set_native_currency(client, native)
+            coinbase_txn = [coinbase_transactions(client, n['id']) for n in coinbase_acc]
+            coinbase_txn = [x for n in coinbase_txn for x in n]
+            # keep only certain transaction types
+            txn_types = ['fiat_deposit', 'request', 'buy', 'fiat_withdrawal', 'vault_withdrawal', 'sell', 'send']
+            coinbase_txn = [n for n in coinbase_txn if n['status'] == 'completed' and n['type'] in txn_types]
+            for d in coinbase_txn:
+                # If the txn is of 'send' type and is a credit, then relabel its type to 'send_credit'
+                if d['type']=='send' and np.sign(float(d['amount']['amount']))==1:
+                    d['type'] = 'send_credit'
+                # If the txn is of 'send' type and is a debit, then relabel its type to 'send_debit' 
+                elif d['type']=='send' and np.sign(float(d['amount']['amount']))==-1:
+                    d['type'] = 'send_debit'
+            
+            # reset native currency
+            coinbase_set_native_currency(client, native)
+            
+            # compute score
+            feedback = create_feedback_coinbase()
+            score, feedback = coinbase_score(coinbase_acc, coinbase_txn, feedback)
+            message = ''
+
+            status_code = 200
+            status = 'Good'
         
-        # compute score
-        feedback = create_feedback_coinbase()
-        score, feedback = coinbase_score(coinbase_acc, coinbase_txn, feedback)
+        except Exception as e:
+            status_code = 400
+            status = 'Error'
+            score = 0
+            feedback = {}
+            message = str(e)
         
-        return score, feedback
-    
-    except Exception as e:
-        return 'Error', str(e)
+        finally:
+            timestamp = datetime.now(timezone.utc).timestamp()
+            output = {
+                'endpoint': '/credit_score/coinbase',
+                'title': 'Credit Score',
+                'status_code': status_code,
+                'status': status,
+                'timestamp': timestamp,
+                'score': score,
+                'feedback': feedback,
+                'message': message
+                }
+        
+        return make_response(output, output['status_code'])
