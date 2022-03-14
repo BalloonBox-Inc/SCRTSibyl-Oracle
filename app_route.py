6aed414fc3ba05b87c89496ffff80651ece3ce09
mayllon <87451748+m3mayllon@users.noreply.github.com>
2022-03-14 11:14:30 -0700

    #33 enable plaid endpoint

--- app_route.py
+++ app_route.py
@@ -24,60 +24,57 @@ def create_feedback_coinbase():
     return {'fetch': {}, 'kyc': {}, 'history': {}, 'liquidity': {}, 'activity': {}}
 
 
-# @app.route('/credit_score/plaid', methods=['POST'])
-@measure_time_and_memory
+# @measure_time_and_memory
+@app.route('/credit_score/plaid', methods=['POST'])
 def credit_score_plaid():
 
-    # if request.method == 'POST':
-    try:
-        # plaid_token = request.json.get('plaid_public_token', None)
-        # keplr_token = request.json.get('keplr_token', None)
-        plaid_token = getenv('PLAID_ACCESS_TOKEN')
-        plaid_client_id = getenv('PLAID_CLIENT_ID')
-        plaid_client_secret = getenv('PLAID_CLIENT_SECRET')
-    
-    except Exception as e:
-        return str(e)
-    
-    try:
-        # client connection
-        client = plaid_client('sandbox', plaid_client_id, plaid_client_secret)
-        # client = plaid_client('production', plaid_client_id, plaid_client_secret)
+    if request.method == 'POST':
+        try:
+            keplr_token = request.json.get('keplr_token', None)
+            plaid_token = request.json.get('plaid_token', None)
+            plaid_client_id = request.json.get('plaid_client_id', None)
+            plaid_client_secret = request.json.get('plaid_client_secret', None)
+        except Exception as e:
+            return str(e)
         
-        # data fetching and formatting
-        plaid_txn = plaid_transactions(plaid_token, client, 360)
-        plaid_txn = {k:v for k,v in plaid_txn.items() if k in ['accounts','transactions']}
-        plaid_txn['transactions'] = [t for t in plaid_txn['transactions'] if not t['pending']]
-        
-        # compute score
-        feedback = create_feedback_plaid()
-        score, feedback = plaid_score(plaid_txn, feedback)
-        message = qualitative_feedback_plaid(score, feedback)
+        try:
+            # client connection
+            client = plaid_client('sandbox', plaid_client_id, plaid_client_secret)
+            # client = plaid_client('production', plaid_client_id, plaid_client_secret)
+            
+            # data fetching and formatting
+            plaid_txn = plaid_transactions(plaid_token, client, 360)
+            plaid_txn = {k:v for k,v in plaid_txn.items() if k in ['accounts','transactions']}
+            plaid_txn['transactions'] = [t for t in plaid_txn['transactions'] if not t['pending']]
+            
+            # compute score
+            feedback = create_feedback_plaid()
+            score, feedback = plaid_score(plaid_txn, feedback)
+            message = qualitative_feedback_plaid(score, feedback)
 
-        status_code = 200
-        status = 'Good'
-    
-    except Exception as e:
-        status_code = 400
-        status = 'Error'
-        score = 0
-        feedback = {}
-        message = str(e)
-    
-    finally:
-        timestamp = datetime.now(timezone.utc).timestamp()
-        output = {
-            'endpoint': '/credit_score/plaid',
-            'title': 'Credit Score',
-            'status_code': status_code,
-            'status': status,
-            'timestamp': timestamp,
-            'score': score,
-            'feedback': feedback,
-            'message': message
-            }
-        return score, feedback, message
-        # return make_response(output, output['status_code'])
+            status_code = 200
+            status = 'Good'
+        
+        except Exception as e:
+            status_code = 400
+            status = 'Error'
+            score = 0
+            feedback = {}
+            message = str(e)
+        
+        finally:
+            timestamp = datetime.now(timezone.utc).timestamp()
+            output = {
+                'endpoint': '/credit_score/plaid',
+                'title': 'Credit Score',
+                'status_code': status_code,
+                'status': status,
+                'timestamp': timestamp,
+                'score': score,
+                'feedback': feedback,
+                'message': message
+                }
+            return make_response(output, output['status_code'])
 
 
 # @app.route('/credit_score/coinbase', methods=['POST'])
