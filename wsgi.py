6ad02e51b213b5073a8a3860b02eedf3302cd82b
Irene Fabris <irene@balloonbox.io>
2022-03-18 10:15:52 -0700

    test remotly -> Heroku

--- wsgi.py
+++ wsgi.py
@@ -2,5 +2,6 @@ from app_route import app
 
 
 if __name__ == '__main__':
-    # app.run(port=7000, debug=True)
-    app.run()
+    # app.run(port=7000, debug=True)   # test locally
+    app.run() # test remotly on Heroku 
+
